f2ba0d2066c5a982fe8bf90d4ff54a39d7753e28
ByRon <amendicovobis29@gmail.com>
2021-05-15 15:19:24 -0500

    first commit

---
+++ frontend/src/App.js
@@ -0,0 +1,63 @@
+import React from "react";
+import data from "./data";
+
+function App() {
+  return (
+    <div className="grid-container">
+      <header className="row">
+        <div>
+          <a className="brand" href="index.html">
+            amazona
+          </a>
+        </div>
+        <div>
+          <a href="cart.html">Cart</a>
+          <a href="signin.html">Sign In</a>
+        </div>
+      </header>
+      <main>
+        <div>
+          <div className="row center">
+            {data.products.map((product) => (
+              <div key={product._id} className="card">
+                <a href={`/product/${product.id}`}>
+                  <img
+                    className="medium"
+                    src={product.image}
+                    alt={product.name}
+                  />
+                </a>
+                <div className="card-body">
+                  <a href={`/product/${product.id}`}>
+                    <h2>Nike Slim Shirts</h2>
+                  </a>
+                  <div className="rating">
+                    <span>
+                      <i className="fa fa-star"></i>
+                    </span>
+                    <span>
+                      <i className="fa fa-star"></i>
+                    </span>
+                    <span>
+                      <i className="fa fa-star"></i>
+                    </span>
+                    <span>
+                      <i className="fa fa-star"></i>
+                    </span>
+                    <span>
+                      <i className="fa fa-star"></i>
+                    </span>
+                  </div>
+                  <div className="price">${product.price}</div>
+                </div>
+              </div>
+            ))}
+          </div>
+        </div>
+      </main>
+      <footer className="row center">All rigths reserved</footer>
+    </div>
+  );
+}
+
+export default App;
